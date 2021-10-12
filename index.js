fec7adbbd32f57da8a51f38031297d5ceecb583e
TanakaRyosei <ryky.11267@gmail.com>
2021-10-12 13:58:17 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const iframeElement = document.getElementById("iframe");
+const iframeElementTitleBar = iframeElement.contentWindow.document.querySelector(".i4ewOd-pzNkMb-haAclf");
+iframeElementTitleBar.style.display = "none";
